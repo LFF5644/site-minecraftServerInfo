bf5344d1f28aefe9549a1607de82d767faa16e3e
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-03-08 13:53:47 +0100

    BETTER CODE!

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let servers=[];
 let serverStatus={};
+let hasServers=false;
+let hasServerStatus=false;
 
 const socket=io({path:"/bind/socket/minecraftServerInfo/socket.io/"});
 const ids={
 	code_connected: document.getElementById("code_connected"),
 	button_connect: document.getElementById("button_connect"),
 	button_disconnect: document.getElementById("button_disconnect"),
-	div_servers: document.getElementById("div_servers"),
+	div_server: document.getElementById("div_server"),
+	select_server: document.getElementById("select_server"),
 };
-const names={
-	radio_servers: document.getElementsByName("radio_servers"),
-}
 
 function updateText(){
 	const connected=socket.connected;
@@ -20,54 +20,80 @@ function updateText(){
 	ids.button_connect.disabled=connected;
 	ids.button_disconnect.disabled=!connected;
 }
-function updateServerHTML(){
-	let html="";
-	for(let index in servers){
-		const server=servers[index];
-		const key=server.id?server.id:String(index);
-		const status=serverStatus[key];
-		html+=(`
-			<p>
-				<label title=${server.id?server.id:"\"\""}>
-					<input type=radio name=radio_servers value="${key}">
-					${server.name}${
-						!status.httpOnline||
-						!status.running?
-						" <span style=\"color:red;\">(OFFLINE)</span>":""
-					}${
-						status.playersOnline?
-						" <span style=\"color:green;\">("+status.playersOnline+" Online)</span>":""
-					}
-				</label>
-			<p>
-			${status.httpOnline?`
-				<div id=div_server_${key} class=hidden>
-						<p>Status: <code${status.statusColor?" style=\"color:"+status.statusColor+";\"":""}>${status.status}</code></p>
-						<p>Spieler Online ${status.playersOnline}: <code>${status.players.join("; ")}</code></p>
-				</div>
-			`:""}
+function setServers(){
+	if(!hasServerStatus||!hasServers) return;
 
-		`
-			.split("\n").join("")
-			.split("\t").join("")
-		);
+	const oldValue=ids.select_server.value;
+	ids.select_server.innerHTML="";
+
+	{
+		let element=document.createElement("option");
+		element.innerText="-- Kein Server --";
+		element.value="$none";
+		ids.select_server.appendChild(element);
+
+		element=document.createElement("option");
+		element.innerText="-- Spieler Übersicht --";
+		element.value="$players";
+		ids.select_server.appendChild(element);
 	}
-	ids.div_servers.innerHTML=html;
-	for(let radio of names.radio_servers){radio.onchange=radioServersChanged}
+	for(let server of servers){
+		const id=server.id;
+		const status=serverStatus[id];
+
+		const element=document.createElement("option");
+		element.innerText=server.name;
+		element.value=id;
+		element.disabled=!status.httpOnline;
+		ids.select_server.appendChild(element);
+	}
+	ids.select_server.value=oldValue;
+	displayServer();
 }
-function findActiveRadio(list){
-	for(let radio of list){
-		if(radio.checked){
-			return radio.value;
+function totalPlayersOnline(){
+	return eval(
+		Object.keys(serverStatus)
+			.map(item=>serverStatus[item].players.length)
+			.join("+")
+	);
+}
+function displayServer(){
+	if(!hasServerStatus||!hasServers) return;
+	const id=ids.select_server.value;
+	if(id.startsWith("$")){
+		const value=id.substring(1);
+		if(value=="none") ids.div_server.innerHTML="";
+		if(value=="players"){
+			const serversWithPlayers=(Object.keys(serverStatus)
+				.filter(item=>
+					serverStatus[item].players.length
+				)
+				.map(item=>
+					[item,serverStatus[item].players]
+				)
+			);
+			const playersOnline=totalPlayersOnline();
+			ids.div_server.innerHTML=`<p>Spieler insgesamt: <span style="color:${playersOnline?"green":"red"}">${playersOnline}</span></p>`;
+			ids.div_server.innerHTML+=(serversWithPlayers
+				.map(item=>
+					`<p>${item[0]} <ul>${item[1]
+						.map(item=>"<li>"+item+"</li>")
+						.join("")
+					}</ul></p>`
+				)
+				.join("")
+			);
 		}
+		return;
 	}
-	throw new Error("in radio list isn't an checked radio element");
-}
-function radioServersChanged(){
-	const key=findActiveRadio(names.radio_servers);
-	const disable=servers.map(item=>item.id);
-	for(let id of disable){document.getElementById("div_server_"+id).className="hidden"}
-	document.getElementById("div_server_"+key).className="";
+	
+	const status=serverStatus[id];
+	const server=servers.find(item=>item.id==id);
+
+	ids.div_server.innerHTML=`<p>Server Name: <span style="color:green">${server.name}</span></p>`;
+	ids.div_server.innerHTML+=`<p>Server id: <span style="color:green">${server.id}</span></p>`
+	ids.div_server.innerHTML+=`<p>Spieler Online: <span style="color:${status.players.length?"green":"red"}">${status.players.length}</p>`;
+	if(status.players.length>0) ids.div_server.innerHTML+=`<p>Spieler: <ul>${status.players.map(item=>`<li style="color:green">${item}</li>`).join("")}</ul></p>`;
 }
 
 ids.button_connect.onclick=event=>{
@@ -78,32 +104,39 @@ ids.button_disconnect.onclick=event=>{
 	const button=event.target;
 	socket.disconnect();
 };
+ids.select_server.onchange=displayServer;
 
 socket.on("connect",()=>{
 	console.log("CONNECTED as "+socket.id);
 	updateText();
 });
 socket.on("get-servers",data=>{
-	console.log("Getting servers!");
-	servers=data; 
+	console.log("Getting: servers");
+	servers=data;
+	hasServers=true;
+	setServers();
 });
 socket.on("get-serverStatus",data=>{
-	console.log("Getting serverStatus");
+	console.log("Getting: serverStatus");
 	serverStatus=data;
+	hasServerStatus=true;
+	setServers();
 });
 socket.on("update-serverStatus",data=>{
 	const {serverKey,status}=data;
 	serverStatus[serverKey]=status;
-	console.log("update server with key "+serverKey);
-	updateServerHTML();
+	if(
+		serverKey==ids.select_server.value||
+		ids.select_server.value.startsWith("$")
+	) displayServer();
+	console.log("Update server: "+serverKey);
 });
-socket.on("update-frame",()=>{
-	updateText();
-	updateServerHTML();
-})
 socket.on("shutdown",code=>{
 	console.log("Shutdown socket server");
 	socket.disconnect();
+});
+socket.on("msg",msg=>{
+	alert(msg);
 });
 socket.on("disconnect",()=>{
 	console.log("DISCONNECTED");
